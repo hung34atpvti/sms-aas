a2683c0725d7b9b9fd482707715cdfa470bbae5f
nmhung <nmhung@cmc.com.vn>
2021-05-03 21:47:41 +0700

    second commit

--- src/main/java/com/cmcg/nmhung/smsaas/SmsAasApplication.java
+++ src/main/java/com/cmcg/nmhung/smsaas/SmsAasApplication.java
@@ -2,8 +2,12 @@ package com.cmcg.nmhung.smsaas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@FeignClient
 public class SmsAasApplication {
 
     public static void main(String[] args) {
