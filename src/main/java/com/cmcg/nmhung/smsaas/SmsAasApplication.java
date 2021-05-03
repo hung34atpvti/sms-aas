90353d00d3a10dacc79f5e09f495ed562ade6764
nmhung <nmhung@cmc.com.vn>
2021-05-03 18:30:16 +0700

    fc

---
+++ src/main/java/com/cmcg/nmhung/smsaas/SmsAasApplication.java
@@ -0,0 +1,13 @@
+package com.cmcg.nmhung.smsaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsAasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmcg/nmhung/smsaas/SmsAasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmcg.nmhung.smsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsAasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
